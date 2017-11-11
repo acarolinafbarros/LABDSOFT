3c58c34fe70098f57699a93e35450ac2870ac5ad
Daniel Bento <1101516@isep.ipp.pt>
2017-11-11 18:42:44 +0000

    Adicionado parte do layout dos questionarios.
    Reference to issue #21

---
+++ GAM/GAM/Controllers/QuestionarioController/QuestionarioController.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using GAM.Data;
+using GAM.Models.Questionarios;
+
+namespace GAM.Controllers.QuestionarioController
+{
+    public class QuestionarioController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public QuestionarioController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Questionario
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Questionario.ToListAsync());
+        }
+
+        // GET: Questionario/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var questionario = await _context.Questionario
+                .SingleOrDefaultAsync(m => m.QuestionarioId == id);
+            if (questionario == null)
+            {
+                return NotFound();
+            }
+
+            return View(questionario);
+        }
+
+        // GET: Questionario/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Questionario/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("QuestionarioId,Area")] Questionario questionario)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(questionario);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(questionario);
+        }
+
+        // GET: Questionario/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var questionario = await _context.Questionario.SingleOrDefaultAsync(m => m.QuestionarioId == id);
+            if (questionario == null)
+            {
+                return NotFound();
+            }
+            return View(questionario);
+        }
+
+        // POST: Questionario/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("QuestionarioId,Area")] Questionario questionario)
+        {
+            if (id != questionario.QuestionarioId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(questionario);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!QuestionarioExists(questionario.QuestionarioId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(questionario);
+        }
+
+        // GET: Questionario/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var questionario = await _context.Questionario
+                .SingleOrDefaultAsync(m => m.QuestionarioId == id);
+            if (questionario == null)
+            {
+                return NotFound();
+            }
+
+            return View(questionario);
+        }
+
+        // POST: Questionario/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var questionario = await _context.Questionario.SingleOrDefaultAsync(m => m.QuestionarioId == id);
+            _context.Questionario.Remove(questionario);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool QuestionarioExists(int id)
+        {
+            return _context.Questionario.Any(e => e.QuestionarioId == id);
+        }
+    }
+}
