63631609ec68e477212714ba8b2c5ac34f6adbf5
Daniel Bento <1101516@isep.ipp.pt>
2017-11-15 23:03:55 +0000

    Criacao e edição de questionarios a funcionar correctamente ref issue #21

--- GAM/GAM/Controllers/QuestionarioController/QuestionarioController.cs
+++ GAM/GAM/Controllers/QuestionarioController/QuestionarioController.cs
@@ -108,17 +108,45 @@ namespace GAM.Controllers.QuestionarioController
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("QuestionarioId,Area")] Questionario questionario)
+        public async Task<IActionResult> Edit(int id, [Bind("QuestionarioId,Area")] Questionario questionario, string perguntasJson)
         {
             if (id != questionario.QuestionarioId)
             {
                 return NotFound();
             }
 
+            var perguntas = JsonConvert.DeserializeObject<List<Pergunta>>(perguntasJson);
+            //questionario.Perguntas = perguntas;
+
             if (ModelState.IsValid)
             {
                 try
                 {
+                    var perguntasId = perguntas.Select(x => x.PerguntaId);
+                    var listaDb = await _context.Pergunta.Where(m => m.QuestionarioId == id).ToListAsync();
+                    //Tratamento das perguntas
+                    foreach (var pergunta in listaDb.ToList())
+                    {
+                        if (perguntasId.Contains(pergunta.PerguntaId))
+                        {
+                            var updatedQuestion = perguntas.SingleOrDefault(x => x.PerguntaId == pergunta.PerguntaId);
+                            pergunta.Descricao = updatedQuestion.Descricao;
+                            pergunta.TipoResposta = updatedQuestion.TipoResposta;
+                            _context.Update(pergunta);
+                        }
+                        else
+                        {
+                            _context.Pergunta.Remove(pergunta);
+                        }
+                    }
+
+                    _context.Pergunta.AddRange(perguntas.Where(x => x.PerguntaId == -1).Select(x=>new Pergunta
+                    {
+                        Descricao = x.Descricao,
+                        QuestionarioId = questionario.QuestionarioId,
+                        TipoResposta = x.TipoResposta
+                    }));
+
                     _context.Update(questionario);
                     await _context.SaveChangesAsync();
                 }
@@ -162,6 +190,7 @@ namespace GAM.Controllers.QuestionarioController
         public async Task<IActionResult> DeleteConfirmed(int id)
         {
             var questionario = await _context.Questionario.SingleOrDefaultAsync(m => m.QuestionarioId == id);
+            _context.Pergunta.RemoveRange(await _context.Pergunta.Where(x=>x.QuestionarioId== id).ToListAsync());
             _context.Questionario.Remove(questionario);
             await _context.SaveChangesAsync();
             return RedirectToAction(nameof(Index));
