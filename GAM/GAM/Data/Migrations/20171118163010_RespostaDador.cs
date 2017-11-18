2380b69695beaaae20c4ae3e8ef5afbd82489509
Tiago Gonçalves [1130739] <1130739@isep.ipp.pt>
2017-11-18 16:32:45 +0000

    Atualização Seed

---
+++ GAM/GAM/Data/Migrations/20171118163010_RespostaDador.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace GAM.Data.Migrations
+{
+    public partial class RespostaDador : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Resposta",
+                table: "Pergunta");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "Apagado",
+                table: "Pergunta",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.CreateTable(
+                name: "Resposta",
+                columns: table => new
+                {
+                    RespostaId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    DadorId = table.Column<int>(type: "int", nullable: false),
+                    PerguntaId = table.Column<int>(type: "int", nullable: false),
+                    TextoResposta = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Resposta", x => x.RespostaId);
+                    table.ForeignKey(
+                        name: "FK_Resposta_Dador_DadorId",
+                        column: x => x.DadorId,
+                        principalTable: "Dador",
+                        principalColumn: "DadorId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Resposta_Pergunta_PerguntaId",
+                        column: x => x.PerguntaId,
+                        principalTable: "Pergunta",
+                        principalColumn: "PerguntaId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Resposta_DadorId",
+                table: "Resposta",
+                column: "DadorId",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Resposta_PerguntaId",
+                table: "Resposta",
+                column: "PerguntaId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Resposta");
+
+            migrationBuilder.DropColumn(
+                name: "Apagado",
+                table: "Pergunta");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Resposta",
+                table: "Pergunta",
+                nullable: true);
+        }
+    }
+}
