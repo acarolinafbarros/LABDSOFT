c57c01ebe6afc5ab4d2e908b86d1e259946966a9
Manuel Correia <1120557@isep.ipp.pt>
2017-11-26 17:44:22 +0000

    Unit tests for PedidoAnalises and PedidoGametas

---
+++ GAM/GamTests/PedidoAnalisesControllerTest.cs
@@ -0,0 +1,96 @@
+﻿using GAM.Controllers.MedicoController;
+using GAM.Data;
+using GAM.Models.DadorViewModels;
+using GAM.Models.Enums;
+using GAM.Models.Laboratorio;
+using GAM.Models.MedicoViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using Xunit;
+
+namespace GamTests
+{
+    public class PedidoAnalisesControllerTest
+    {
+        private static ApplicationDbContext context;
+        private PedidoAnaliseController _pedidoAnalisesController = new PedidoAnaliseController(GetContextWithoutData());
+
+        private static ApplicationDbContext GetContextWithoutData()
+        {
+            var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseInMemoryDatabase(Guid.NewGuid().ToString())
+                .Options;
+
+            context = new ApplicationDbContext(options);
+
+            var dador = new Dador
+            {
+                DadorId = 1,
+                Nome = "Marcelo Moreno",
+                Morada = "Praça D. Joao II",
+                DataNasc = DateTime.UtcNow,
+                LocalNasc = "Amarante",
+                DocIdentificacao = "123987456",
+                Nacionalidade = "Portugal",
+                Profissao = "Engenheiro Quimico",
+                GrauEscolaridade = GrauEscolaridadeEnum.Mestrado,
+                EstadoCivil = EstadoCivilEnum.Viuvo,
+                NumFilhos = 0,
+                Altura = 185,
+                Peso = 78,
+                CorPele = "Branco",
+                CorOlhos = "Verde",
+                CorCabelo = "Preto",
+                TexturaCabelo = "Liso",
+                GrupoSanguineo = GrupoSanguineoEnum.BNeg,
+                Etnia = "Apache",
+                IniciaisDador = "MM",
+                FaseDador = FaseDadorEnum.PrimeiraDadiva,
+                EstadoDador = EstadoDadorEnum.ProcessoInativo,
+                DadosDador = ValidacaoEnum.Aceite,
+                NumAbortos = 0,
+                TotalGestacoes = 0
+            };
+
+            context.Add(dador);
+            context.SaveChanges();
+
+            var amostra = new Amostra
+            {
+                DadorId = 1,
+                EstadoAmostra = EstadoAmostraEnum.PorAnalisar,
+                TipoAmostra = TipoAmostraEnum.Sangue,
+                DataRecolha = DateTime.UtcNow,
+                Banco = GamEnums.TipoBancoEnum.Indefinido,
+                Piso = GamEnums.PisoEnum.Indefinido,
+                Cannister = GamEnums.CannisterEnum.Indefinido,
+                GlobetCor = GamEnums.GlobetCorEnum.Indefinido,
+                GlobetNumero = GamEnums.GlobetNumeroEnum.Indefinido,
+                PalhetaCor = GamEnums.PalhetaCorEnum.Indefinido
+            };
+
+            context.Add(amostra);
+            context.SaveChanges();
+
+            return context;
+        }
+
+        [Fact]
+        public void TestIndex()
+        {
+            // Act
+            var actionResultTask = _pedidoAnalisesController.Index();
+            var viewResult = actionResultTask as ViewResult;
+
+            // Assert
+            Assert.NotNull(viewResult);
+            Assert.NotNull(viewResult.ViewData.Model); // add additional checks on the Model
+            Assert.True(string.IsNullOrEmpty(viewResult.ViewName) || viewResult.ViewName == "Index");
+
+            var model = Assert.IsAssignableFrom<IEnumerable<PedidoAnaliseViewModel>>(viewResult.Model);
+            Assert.Single(model);
+        }
+    }
+}
---
+++ GAM/GamTests/PedidoGametasControllerTest.cs
@@ -0,0 +1,102 @@
+﻿using GAM.Controllers.PMAController;
+using GAM.Data;
+using GAM.Models;
+using GAM.Models.Enums;
+using GAM.Models.PMAViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using Xunit;
+
+namespace GamTests
+{
+    public class PedidoGametasControllerTest
+    {
+        private static ApplicationDbContext context;
+        private PedidoGametasController _pedidoGametasController = new PedidoGametasController(GetContextWithoutData());
+
+        private static ApplicationDbContext GetContextWithoutData()
+        {
+            var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseInMemoryDatabase(Guid.NewGuid().ToString())
+                .Options;
+
+            context = new ApplicationDbContext(options);
+
+            var novoCasal = new Casal
+            {
+                CasalID = 1,
+                IdadeHomem = 45,
+                RacaHomem = "Latino",
+                AlturaHomem = 177,
+                CorCabeloHomem = "Castanho",
+                GrupoSanguineoHomem = GrupoSanguineoEnum.ANeg,
+                TexturaCabeloHomem = "Liso",
+                CorOlhosHomem = "Castanho",
+                CorPeleHomem = "Moreno",
+
+                IdadeMulher = 38,
+                RacaMulher = "Latino",
+                AlturaMulher = 155,
+                CorCabeloMulher = "Loiro",
+                GrupoSanguineoMulher = GrupoSanguineoEnum.ABNeg,
+                TexturaCabeloMulher = "Ondulado",
+                CorOlhosMulher = "Azul",
+                CorPeleMulher = "Branco"
+            };
+
+            context.Add(novoCasal);
+            context.SaveChanges();
+
+            var novoPedidoGametas = new PedidoGametas
+            {
+                PedidoGametasId = 1,
+                CasalId = 1,
+                Data = DateTime.UtcNow,
+                Centro = "Centro PMA Leiria",
+                RefExterna = "PMLEI2002"
+            };
+
+            context.Add(novoPedidoGametas);
+            context.SaveChanges();
+
+            return context;
+        }
+
+        [Fact]
+        public void TestIndex()
+        {
+            // Act
+            var actionResultTask = _pedidoGametasController.Index();
+            actionResultTask.Wait();
+            var viewResult = actionResultTask.Result as ViewResult;
+
+            // Assert
+            Assert.NotNull(viewResult);
+            Assert.NotNull(viewResult.ViewData.Model); // add additional checks on the Model
+            Assert.True(string.IsNullOrEmpty(viewResult.ViewName) || viewResult.ViewName == "Index");
+
+            var model = Assert.IsAssignableFrom<IEnumerable<PedidoGametasViewModel>>(viewResult.Model);
+            Assert.Single(model);
+        }
+
+        [Fact]
+        public void TestDetails()
+        {
+            // Act
+            var actionResultTask = _pedidoGametasController.Details(1);
+            actionResultTask.Wait();
+            var viewResult = actionResultTask.Result as ViewResult;
+
+            // Assert
+            Assert.NotNull(viewResult);
+            Assert.NotNull(viewResult.ViewData.Model); // add additional checks on the Model
+            Assert.True(string.IsNullOrEmpty(viewResult.ViewName) || viewResult.ViewName == "Details");
+
+            var model = Assert.IsAssignableFrom<PedidoGametasViewModel>(viewResult.Model);
+            Assert.Equal("Centro PMA Leiria", model.Centro);
+        }
+     
+    }
+}
